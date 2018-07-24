02e5d01b12cee16c0996ac2125a56d557d41c1c5
Stephane Eybert <mittiprovence@yahoo.se>
2018-07-24 11:50:01 +0300

    Updated the deprecated annotations.

--- src/main/java/com/thalasoft/user/rest/service/resource/CredentialsResource.java
+++ src/main/java/com/thalasoft/user/rest/service/resource/CredentialsResource.java
@@ -1,7 +1,8 @@
 package com.thalasoft.user.rest.service.resource;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.hateoas.ResourceSupport;
 
 public class CredentialsResource extends ResourceSupport {
