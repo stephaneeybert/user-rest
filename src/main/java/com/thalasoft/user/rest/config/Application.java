d587d024ef07dae8b1ceba8aaf6bcbde99e51a09
Stephane Eybert <mittiprovence@yahoo.se>
2018-08-04 23:41:36 +0300

    Added a command line runner for potential future use.

--- src/main/java/com/thalasoft/user/rest/config/Application.java
+++ src/main/java/com/thalasoft/user/rest/config/Application.java
@@ -2,15 +2,20 @@ package com.thalasoft.user.rest.config;
 
 import com.thalasoft.toolbox.condition.EnvProd;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnvProd
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-      }
-    
+    }
+
+    @Override
+    public void run(String... params) throws Exception {
+    }
+
 }
