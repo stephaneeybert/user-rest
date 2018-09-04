8acd74a158033c681f51a0c811a491eccd54025e
Stephane Eybert <mittiprovence@yahoo.se>
2018-09-04 16:07:38 +0200

    Removed an unused import.

--- src/main/java/com/thalasoft/user/rest/controller/AuthenticationController.java
+++ src/main/java/com/thalasoft/user/rest/controller/AuthenticationController.java
@@ -22,7 +22,6 @@ import com.thalasoft.user.rest.utils.RESTConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
