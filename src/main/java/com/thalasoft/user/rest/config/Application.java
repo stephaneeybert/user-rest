adce5e84dc14f336e52451be77fdf9b667042dce
Stephane Eybert <mittiprovence@yahoo.se>
2018-09-11 14:37:09 +0200

    Added Lombok logging. Added a log statement.

--- src/main/java/com/thalasoft/user/rest/config/Application.java
+++ src/main/java/com/thalasoft/user/rest/config/Application.java
@@ -1,21 +1,41 @@
 package com.thalasoft.user.rest.config;
 
+import java.util.Arrays;
+
 import com.thalasoft.toolbox.condition.EnvProd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @EnvProd
 @SpringBootApplication
+@Slf4j
 public class Application implements CommandLineRunner {
+
+    private static final String FIXTURE = "fixture";
+
+    @Autowired
+    private UserFixtureService userFixtureService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
-    public void run(String... params) throws Exception {
+    public void run(String... args) throws Exception {
+        log.info("Application started with command-line arguments: {} .\nTo kill this application, press Ctrl + C.",
+                Arrays.toString(args));
+        if (null != args && args.length > 0) {
+            String dataFixture = args[0];
+            if (dataFixture.equals(FIXTURE)) {
+                userFixtureService.addUser();
+                log.info("Added the data fixture");
+            }
+        }
     }
 
 }
