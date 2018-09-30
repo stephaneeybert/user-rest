e51ff2b6df36cfc468d2eb4da91ff50d2686a138
Stephane Eybert <mittiprovence@yahoo.se>
2018-09-30 19:58:18 +0200

    Removed an unused import.

--- src/main/java/com/thalasoft/user/rest/controller/RootController.java
+++ src/main/java/com/thalasoft/user/rest/controller/RootController.java
@@ -15,7 +15,6 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
