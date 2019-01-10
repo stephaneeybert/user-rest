188a330290348f89e7cf5edab8bfd23482962b5d
Stephane Eybert <mittiprovence@yahoo.se>
2019-01-10 17:56:04 +0200

    Use an explicit name for the user collection under the _embedded json property.

--- src/main/java/com/thalasoft/user/rest/utils/RESTConstants.java
+++ src/main/java/com/thalasoft/user/rest/utils/RESTConstants.java
@@ -2,26 +2,26 @@ package com.thalasoft.user.rest.utils;
 
 public final class RESTConstants {
 
-	public static final String SLASH = "/";
-	public static final String API = "api";
-	public static final String NOT = "not";
-    public static final String COUNT = "count";
-    public static final String DEPENDENCIES = "dependencies";
-    public static final String RESOURCES = "resources";
+  public static final String SLASH = "/";
+  public static final String API = "api";
+  public static final String NOT = "not";
+  public static final String COUNT = "count";
+  public static final String DEPENDENCIES = "dependencies";
+  public static final String RESOURCES = "resources";
+  public static final String EMBEDDED_COLLECTION_NAME_USER = "userResourceList";
 
-    public static final String METRICS = "metrics";
-    public static final String GRAPH = "graph";
+  public static final String METRICS = "metrics";
+  public static final String GRAPH = "graph";
 
-    public static final String REL_FIRST = "first";
-    public static final String REL_PREV = "prev";
-    public static final String REL_NEXT = "next";
-    public static final String REL_LAST = "last";
+  public static final String REL_FIRST = "first";
+  public static final String REL_PREV = "prev";
+  public static final String REL_NEXT = "next";
+  public static final String REL_LAST = "last";
 
-    public static final String PAGEABLE_SORT_SUFFIX = ".dir";
+  public static final String PAGEABLE_SORT_SUFFIX = ".dir";
 
-    private RESTConstants() {
-        throw new AssertionError();
-    }
-    
+  private RESTConstants() {
+    throw new AssertionError();
+  }
+
 }
-
