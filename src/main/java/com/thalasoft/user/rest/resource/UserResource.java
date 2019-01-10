6191e1ba4a3d1c9c8c8f8be17f716e92a57f4fbd
Stephane Eybert <mittiprovence@yahoo.se>
2019-01-10 17:56:04 +0200

    Use an explicit name for the user collection under the _embedded json property.

--- src/main/java/com/thalasoft/user/rest/resource/UserResource.java
+++ src/main/java/com/thalasoft/user/rest/resource/UserResource.java
@@ -9,24 +9,29 @@ import javax.validation.constraints.NotEmpty;
 
 import lombok.*;
 
+import com.thalasoft.user.rest.utils.RESTConstants;
+
+import org.springframework.hateoas.core.Relation;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Relation(collectionRelation = RESTConstants.EMBEDDED_COLLECTION_NAME_USER)
 public class UserResource extends AbstractResource {
 
-    @NotEmpty
-    private String firstname;
-    @NotEmpty
-    private String lastname;
-    @NotEmpty
-    @Email
-    private String email;
-    private boolean confirmedEmail;
-    private String password;
-    private String workPhone;
-    @Valid
-    private Set<UserRoleResource> userRoleResources = new HashSet<UserRoleResource>();
+  @NotEmpty
+  private String firstname;
+  @NotEmpty
+  private String lastname;
+  @NotEmpty
+  @Email
+  private String email;
+  private boolean confirmedEmail;
+  private String password;
+  private String workPhone;
+  @Valid
+  private Set<UserRoleResource> userRoleResources = new HashSet<UserRoleResource>();
 
 }
